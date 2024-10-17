2fbea081006236100f38755c23f4393914421545
Raillo1 <raillo.romero.jesus.ramon@iescamas.es>
2024-10-17 09:06:15 +0200

    Se ha agregado en la carpeta Modelo además de corregir la ruta de las clases importadas

--- Prueba.php
+++ Prueba.php
@@ -1,7 +1,8 @@
 <?php
-require_once 'Cliente.php';
-require_once 'ProductoFisico.php';
-//Este archivo se utiliza para comprobar elcodigo deotras clases que esté correctamente y bien implementado
+require_once 'Modelo/Cliente.php';
+require_once 'Modelo/ProductoFisico.php';
+//Este archivo se utiliza para comprobar el codigo de otras clases que esté correctamente y bien implementado
+
 require_once 'Cliente.php';
     $c1 = new Cliente("Pablo Mayo");
     $c2 = new Cliente("Rocio Gonzalez");
